b04df12a73cf7cde705434f360288024203a56aa
mreeks <mreeks@macalester.edu>
2012-11-01 14:32:29 -0500

    text parser

--- src/edu/macalester/textParser.java
+++ src/edu/macalester/textParser.java
@@ -8,4 +8,15 @@ package edu.macalester;
  * To change this template use File | Settings | File Templates.
  */
 public class textParser {
+	public void textContent(String sms,String passcode){
+		String[] res = sms.split("//s+");
+		boolean passphrase = false;
+		for(int i=0; i<res.length; i++){
+			if(res[i].equals(passcode)){
+				passphrase = true;
+				res[i] = null;
+			}
+		}
+		//if(passphrase) TriangulumMain.something(res);
+	}
 }
