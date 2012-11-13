6260859e35de646743e7f15a55ace413c06fea89
aaronlaursen <aaronlaursen@gmail.com>
2012-11-13 15:28:20 -0600

    working alert

--- src/edu/macalester/modules/alert/alert.java
+++ src/edu/macalester/modules/alert/alert.java
@@ -1,23 +1,28 @@
 package edu.macalester.modules.alert;
 
 import android.media.MediaPlayer;
+import edu.macalester.R;
 import edu.macalester.modules.triangulumModule;
 
-import java.io.IOException;
-
-class alert extends triangulumModule{
+public class alert extends triangulumModule{
     public String getTxt(){
 
-        /*SoundPool pool = new SoundPool(4, AudioManager.STREAM_MUSIC, 0);
-        int s = pool.play(R.raw.alert, (float)1.0,(float)1.0,);
-        pool.play(s,(float)1.0,(float)1.0,1,0,(float)1.0);*/
+        //SoundPool pool = new SoundPool(4, AudioManager.STREAM_MUSIC, 0);
+        //int sound = pool.load("/src/edu/macalester/modules/alert/trousers.mp3",1);
+        //pool.play(sound, 1.0f, 1.0f, 0, 0, 1.0f);
 
-        MediaPlayer mp = new MediaPlayer();
-        try{
-            mp.setDataSource("src/edu/macalester/modules/alert/trousers.mp3");
+
+        MediaPlayer mp = MediaPlayer.create(context, R.raw.alert);
+        mp.start();
+        /*try{
+            //File mFile = new File("src/edu/macalester/modules/alert/trousers.mp3");
+            //FileInputStream fs = new FileInputStream(mFile);
+            //mp.setDataSource(fs.getFD());
+            mp.setDataSource(context.getApplicationContext().getFilesDir()+"src/edu/macalester/modules/alert/trousers.mp3");
+            mp.start();
         } catch (IOException e){
             return"fail";
-        }
+        }*/
         return "Success";
     }
 }
