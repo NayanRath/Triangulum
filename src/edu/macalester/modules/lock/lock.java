3a2cc8e7bf3e85166693f8b9a9615e70408f201d
aaronlaursen <aaronlaursen@gmail.com>
2012-11-27 13:42:23 -0600

    lock resets password to random 6 characters

--- src/edu/macalester/modules/lock/lock.java
+++ src/edu/macalester/modules/lock/lock.java
@@ -6,9 +6,13 @@ import android.content.Context;
 import edu.macalester.modules.triangulumModule;
 import edu.macalester.triangulumAdminReceiver;
 
+import java.util.Date;
+import java.util.Random;
+
 public class lock extends triangulumModule {
     private DevicePolicyManager manager;
     ComponentName devAdm;
+    String newPass;
 	public String getTxt(){
         //PowerManager manager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
 		//manager.goToSleep(0);
@@ -18,8 +22,21 @@ public class lock extends triangulumModule {
         manager=(DevicePolicyManager)context.getSystemService(Context.DEVICE_POLICY_SERVICE);
         devAdm = new ComponentName(context, triangulumAdminReceiver.class);
         //DeviceAdminReceiver adRec = new DeviceAdminReceiver();
+        //manager.resetPassword(,0);
 
+        Character[] validCharacters = {'q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z',
+                'x','c','v','b','n','m','Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z',
+                'X','C','V','B','N','M','1','2','3','4','5','6','7','8','9','0'};
+        String newPass="";
+        Random rand = new Random((new Date()).getDate());
+        for (int i=0;i<6;i++){
+            newPass+=validCharacters[rand.nextInt(validCharacters.length)];
+        }
 
+        //Note: uncomment the following line for hardcoded newPass
+        //newPass="asdf";
+
+        manager.resetPassword(newPass, 0);
         manager.lockNow();
 
 /*        KeyguardManager mgr = (KeyguardManager)context.getSystemService(context.KEYGUARD_SERVICE);
@@ -27,7 +44,7 @@ public class lock extends triangulumModule {
         lock.disableKeyguard();
         lock.reenableKeyguard();*/
 
-        String txt = "worked?";
+        String txt = "Device Locked: "+newPass;
 		return txt;
 
 	}
